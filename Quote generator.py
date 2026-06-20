2b67fd5c2660801ec224fa4326c5f125b586f56e
GhostyBeat <glebbytch@yandex.com>
2026-06-20 21:52:30 +0700

    Add files via upload

---
+++ Quote generator.py
@@ -0,0 +1,122 @@
+import random
+import json
+import tkinter as tk
+import Quote_adder
+
+#Палитра фонов
+main_background = '#fff0c8'
+labels_background = '#c89650'
+text_background = '#fff0dc'
+history = []
+try:
+    with open('Quotes history.json', encoding='utf-8') as file:
+        history = json.load(file)
+except FileNotFoundError: ''
+except json.decoder.JSONDecodeError: ''
+
+#Функции для работы
+def update():
+    global quotes
+    try:
+        file = open('Quotes list.json', encoding='utf-8')
+        quotes = json.load(file)
+        file.close()
+        for author in authors: author_listbox.insert(tk.END, author)
+        for theme in themes: themes_listbox.insert(tk.END, theme)
+    except FileNotFoundError: ''
+    except json.decoder.JSONDecodeError: ''
+
+def select_authors():
+    global choiced_authors
+    choiced_authors = []
+    for i in reversed(author_listbox.curselection()):
+        choiced_authors.append(author_listbox.get(i))
+    if choiced_authors: authors_selection_label['text'] = 'Автор(ы) выбраны'
+    else: authors_selection_label['text'] = ''
+
+def select_themes():
+    global choiced_themes
+    choiced_themes = []
+    for i in reversed(themes_listbox.curselection()):
+        choiced_themes.append(themes_listbox.get(i))
+    if choiced_themes: themes_selection_label['text'] = 'Тема(ы) выбраны'
+    else: themes_selection_label['text'] = ''
+
+def generate():
+    global choiced_authors, choiced_themes
+    variants = []
+    if choiced_themes and choiced_authors:
+        for quote in quotes:
+            if quote['theme'] in choiced_themes and quote['author'] in choiced_authors:
+                variants.append({'author':quote['author'], 'theme':quote['theme'], 'text':quote['text']})
+    elif choiced_themes and not choiced_authors:
+        for quote in quotes:
+            if quote['theme'] in choiced_themes:
+                variants.append({'author': quote['author'], 'theme': quote['theme'], 'text': quote['text']})
+    elif not choiced_themes and choiced_authors:
+        for quote in quotes:
+            if quote['author'] in choiced_authors:
+                variants.append({'author': quote['author'], 'theme': quote['theme'], 'text': quote['text']})
+    else:
+        for quote in quotes:
+            variants.append({'author': quote['author'], 'theme': quote['theme'], 'text': quote['text']})
+
+    if variants:
+        res_quote = random.choice(variants)
+        res = f'"{res_quote['text']}" \n {res_quote["author"]}'
+        res_text.delete('1.0', tk.END)
+        res_text.insert('1.0', res)
+        history.append(f'"{res_quote['text']}" - {res_quote["author"]}')
+        with open('Quotes history.json', 'w', encoding='utf-8') as f:
+            json.dump(history, f, indent=4, ensure_ascii=False)
+    else:
+        res_text.delete('1.0', tk.END)
+        res_text.insert('1.0', 'Цитата не найдена')
+
+    choiced_authors, choiced_themes = [], []
+
+
+def adding_window(): Quote_adder.adding_window()
+def history_window(): Quote_adder.history_window()
+
+quotes ,authors, themes, choiced_themes, choiced_authors, author_listbox, themes_listbox = [], [], [], [], [], [], []
+update()
+for quote in quotes:
+    authors.append(quote['author'])
+    themes.append(quote['theme'])
+authors = sorted(list(set(authors)))
+themes = sorted(list(set(themes)))
+
+
+showing_window = tk.Tk()
+showing_window.title('Генератор случайных цитат')
+showing_window.geometry('600x500')
+showing_window.minsize(600, 500)
+showing_window.configure(bg = main_background)
+
+tk.Label(showing_window, bg=labels_background, fg = 'white' , font = 'Arial 13' ,text = 'Выберите фильтры для отображения определенных цитат (авторы и тема)').pack(pady = 10, fill = tk.X)
+authors_selection_label = tk.Label(showing_window, fg = 'black', text = '', bg = main_background)
+themes_selection_label = tk.Label(showing_window, fg = 'black', text= '', bg = main_background)
+authors_selection_label.place(relx=0.25, y=50, anchor = 'center')
+themes_selection_label.place(relx=0.75, y=50, anchor = 'center')
+
+author_listbox = tk.Listbox(showing_window, width=40, height=10, selectmode='multiple', bg = text_background)
+author_listbox.place(relx=0.25, y = 150, anchor = 'center')
+
+themes_listbox = tk.Listbox(showing_window, width=40, height=10, selectmode='multiple', bg = text_background)
+themes_listbox.place(relx=0.75, y = 150, anchor = 'center')
+
+for author in authors: author_listbox.insert(tk.END, author)
+for theme in themes: themes_listbox.insert(tk.END, theme)
+
+tk.Button(command=select_authors, text='Выбрать авторов', relief='flat', bg= labels_background, fg ='white').place(relx=0.25, y= 250, anchor = 'center')
+tk.Button(command=select_themes, text='Выбрать темы', relief='flat', bg= labels_background, fg ='white').place(relx=0.75, y= 250, anchor = 'center')
+tk.Button(command=generate, text='Сказать что-нибудь', relief='flat', bg= labels_background, width=2360, font= 'Arial 13', fg = 'white').place(relx=0.5, y= 300, anchor = 'center')
+res_text = tk.Text(showing_window, bg = text_background, height= 6, font= 'Arial 11', width= 70, wrap='word')
+res_text.place(relx=0.5, y = 330, anchor = 'n')
+
+tk.Button(text = 'Добавить от себя', relief='flat', bg= labels_background, fg ='white', command=adding_window).place(x=290, y= 465, anchor = 'e')
+tk.Button(text = 'История', relief='flat', bg= labels_background, fg ='white', command=history_window).place(x=310, y= 465, anchor = 'w')
+
+
+showing_window.mainloop()
---
+++ Quote_adder.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+import json
+main_background = '#fff0c8'
+labels_background = '#c89650'
+text_background = '#fff0dc'
+
+quote_theme, quote_author, quote_text, add_theme, add_text, add_author, res_label, history = '','','','','','','',''
+
+
+def history_window():
+    global history
+    history_text = ''
+    try:
+        with open('Quotes history.json', 'r', encoding='utf-8') as file:
+            history = json.load(file)
+            for hi in history: history_text += hi + '\n'
+    except FileNotFoundError: history_text = 'История отсутствует.'
+    except json.decoder.JSONDecodeError: history_text = 'История отсутствует.'
+
+    history_window = tk.Tk()
+    history_window.geometry('600x300')
+    history_window.minsize(600, 240)
+    history_window.resizable(False, False)
+    history_window.configure(bg=main_background)
+    history_window.title('История цитат')
+    tk.Label(history_window, bg=labels_background, fg='white', font='Arial 13', text='История').pack(pady=10, fill=tk.X)
+    his_text = tk.Text(history_window, bg=text_background, height=6, font='Arial 11', width=70, wrap='word')
+    his_text.pack(pady=10)
+    his_text.insert('1.0', history_text)
+
+
+
+def add_quote():
+    quote_theme = add_theme.get()
+    quote_text = add_text.get()
+    quote_author = add_author.get()
+    add_theme.delete(0, tk.END)
+    add_text.delete(0, tk.END)
+    add_author.delete(0, tk.END)
+    if quote_theme and quote_text and quote_author:
+        quote = {'author': quote_author, 'text': quote_text, 'theme': quote_theme}
+        try:
+            with open('Quotes list.json', 'r', encoding='utf-8') as file:
+                quotes = json.load(file)
+            quotes.append(quote)
+            with open('Quotes list.json', 'w', encoding='utf-8') as file:
+                json.dump(quotes, file, indent=4, sort_keys=True, ensure_ascii=False)
+            res_label['text'] = 'Добавлено'
+        except FileNotFoundError: ''
+        except json.decoder.JSONDecodeError: ''
+    else: res_label['text'] = 'Не все поля заполнены'
+
+
+def adding_window():
+    global add_theme, add_author, add_text, res_label
+    adding_window = tk.Tk()
+    adding_window.title('Добавить цитату')
+    adding_window.geometry('600x240')
+    adding_window.minsize(600, 240)
+    adding_window.resizable(False, False)
+    adding_window.configure(bg=main_background)
+    tk.Label(adding_window, bg=labels_background, fg='white', font='Arial 13', text='Заполните все поля для добавления').pack(pady=10, fill=tk.X)
+
+    tk.Label(adding_window, bg=main_background, fg='black', font='Arial 11', text='Тема:').place(y=50, anchor='w', x=10)
+    tk.Label(adding_window, bg=main_background, fg='black', font='Arial 11', text='Цитата:').place(y=80, anchor='w', x=10)
+    tk.Label(adding_window, bg=main_background, fg='black', font='Arial 11', text='Автор:').place(y=110, anchor='w', x=10)
+
+    add_theme = tk.Entry(adding_window, bg=text_background, fg='black', font='Arial 11', width=50)
+    add_text = tk.Entry(adding_window, bg=text_background, fg='black', font='Arial 11', width=50)
+    add_author = tk.Entry(adding_window, bg=text_background, fg='black', font='Arial 11', width=50)
+
+    res_label = tk.Label(adding_window, bg=labels_background, fg='white', font='Arial 13', text='', width=90)
+    tk.Button(adding_window, command=add_quote, text='Добавить', relief='flat', bg=labels_background, width=2360, font='Arial 13', fg='white').place(relx=0.5, y=190, anchor='center')
+
+    add_theme.place(x = 70, y = 50, anchor='w')
+    add_text.place(x = 70, y = 80, anchor='w')
+    add_author.place(x = 70, y = 110, anchor='w')
+    res_label.place(relx = 0.5, y = 140, anchor='n')
+
+    adding_window.mainloop()
